03dd7ea3af9d65e7374d38b4c2ff433335fe387f
Eduardo Pessine <eduardopessine@gmail.com>
2020-11-15 18:55:26 -0300

    adiciona js do carousel

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ function scrollFunction() {
   }
 }   
 // muda o background do header no scroll
+
+//carousel
+
+function right(car) {
+  var pai = car.parentElement;
+  var nome = pai.children[2];
+  var valor = 6
+  var tempo = setInterval(move, 1);
+  function move() {
+    if (valor == 96) {
+      clearInterval(tempo);
+    } else {
+      valor = valor + 2;
+      nome.style.left = "-" + valor + "vw";
+    }
+  }
+  document.querySelector(".carousel-next").style.visibility = "hidden";
+  document.querySelector(".carousel-previous").style.visibility = "visible";
+}
+
+function left(car) {
+  var pai = car.parentElement;
+  var nome = pai.children[2];
+  var valor = 96
+  var tempo = setInterval(move, 1);
+  function move() {
+    if (valor == 6) {
+      clearInterval(tempo);
+    } else {
+      valor = valor - 2;
+      nome.style.left = "-" + valor + "vw";
+    }
+  }
+  document.querySelector(".carousel-next").style.visibility = "visible";
+  document.querySelector(".carousel-previous").style.visibility = "hidden";
+}
+
+// carousel
